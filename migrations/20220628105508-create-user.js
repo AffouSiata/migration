49394219c9dd63c3fb5e66fd08e2827ac9fae845
AffouSiata <affousiatao59@gmail.com>
2022-07-01 18:20:21 +0000

    encore commit

--- migrations/20220628105508-create-user.js
+++ migrations/20220628105508-create-user.js
@@ -9,15 +9,42 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      firstName: {
-        type: Sequelize.STRING
+      firstName:{
+        type:Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+          validate: {
+              len:{
+                    args: 3,
+                    msg: "Le nom doit comporter au moins 3 caractères"
+                  }
+              }
       },
-      lastName: {
-        type: Sequelize.STRING
+      lastName:{
+        type:Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+          validate: {
+              len:{
+                    args: 4,
+                    msg: "Le prenom doit comporter au moins 3 caractères"
+                  }
+              }
       },
       email: {
-        type: Sequelize.STRING
-      },
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: {
+                args: [6, 128],
+                msg: "L'adresse e-mail doit comporter entre 6 et 128 caractères"
+            },
+            isEmail: {
+                msg: "L'adresse e-mail doit être valide"
+            }
+        }
+    },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
@@ -27,7 +54,14 @@ module.exports = {
         type: Sequelize.DATE
       }
     });
+    Model.sync().success(function() {
+      Model.build({ from: "Users" }).validate().success(function(errors) {
+        console.log(errors)
+      })
+    })
+    
   },
+ 
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
   }
